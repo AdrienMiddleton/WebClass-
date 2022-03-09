e257cf664f26c3bde4cb0d373d5b58e5cc3c10af
AdrienMiddleton <97462092+AdrienMiddleton@users.noreply.github.com>
2022-03-09 16:06:04 -0600

    Create navigation.js

---
+++ Module4/Module4Exercise/navigation.js
@@ -0,0 +1,8 @@
+function openlist(){
+    document.getElementById("content2").style.left = "0";
+    document.getElementById("content2").style.opacity = "1";
+}
+function closelist(){
+    document.getElementById("content2").style.left = "-100%";
+    document.getElementById("content2").style.opacity = "0";
+}
